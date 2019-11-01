7bebbd9f6e68e16908cbdf92ac33f80577d06733
Marian  Patak <marian@patak.sk>
2019-11-01 16:47:24 +0100

    initial commit

---
+++ src/payqrcode.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Mcfarlow\Payqrcode;
+
+class Payqrcode
+{
+    public function greet(String $sName)
+    {
+        return 'Hi ' . $sName . '! How are you doing today?';
+    }
+}
